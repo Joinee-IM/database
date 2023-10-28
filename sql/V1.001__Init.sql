860988612b440656edd48a1c727b703a7a0ab70e
wsa <bensonwang2001@gmail.com>
2023-10-28 23:04:39 +0800

    v1.001: init sql

---
+++ sql/V1.001__Init.sql
@@ -0,0 +1,175 @@
+CREATE TABLE gcs_file
+(
+    file_uuid UUID PRIMARY KEY,
+    bucket    VARCHAR NOT NULL,
+    key       VARCHAR NOT NULL,
+    filename  VARCHAR NOT NULL
+);
+
+CREATE TYPE gender_type AS ENUM (
+    'Male',
+    'Female',
+    'Unrevealed'
+);
+
+CREATE TYPE fee_type AS ENUM (
+    'PER_HOUR',
+    'PER_PERSON',
+    'PER_PERSON_PER_HOUR'
+);
+
+CREATE TYPE place_type AS ENUM (
+    'STADIUM',
+    'VENUE'
+);
+
+CREATE TYPE technical_type AS ENUM (
+    'ENTRY',
+    'INTERMEDIATE',
+    'ADVANCED'
+);
+
+CREATE TYPE role_type AS ENUM (
+    'PROVIDER',
+    'NORMAL'
+);
+
+
+CREATE TABLE account
+(
+    id              SERIAL PRIMARY KEY,
+    email           VARCHAR UNIQUE NOT NULL,
+    pass_hash       VARCHAR        NOT NULL,
+    nickname        VARCHAR,
+    gender          gender_type    NOT NULL,
+    access_token    VARCHAR,
+    refresh_token   VARCHAR,
+    image_uuid      UUID REFERENCES gcs_file (file_uuid),
+    role            role_type      NOT NULL,
+    is_google_login BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE email_verification
+(
+    code        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email       VARCHAR NOT NULL,
+    account_id  INTEGER NOT NULL REFERENCES account (id),
+    is_consumed BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE sport
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE city
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE district
+(
+    id      SERIAL PRIMARY KEY,
+    city_id INTEGER NOT NULL REFERENCES city (id),
+    name    VARCHAR NOT NULL
+);
+
+CREATE TABLE stadium
+( -- 場館
+    id             SERIAL PRIMARY KEY,
+    name           VARCHAR NOT NULL,
+    district_id    INTEGER NOT NULL REFERENCES district (id),
+    contact_number VARCHAR,
+    description    TEXT,
+    long           FLOAT   NOT NULL,
+    lat            FLOAT   NOT NULL
+);
+
+CREATE TABLE venue
+( -- 場地
+    id                   SERIAL PRIMARY KEY,
+    stadium_id           INTEGER NOT NULL REFERENCES stadium (id),
+    name                 VARCHAR NOT NULL,
+    floor                VARCHAR NOT NULL,
+    reservation_interval INTEGER,
+    fee_rate             FLOAT,
+    fee_type             fee_type,
+    area                 INTEGER NOT NULL,
+    capability           INTEGER NOT NULL,
+    sport_equipments     VARCHAR,
+    facilities           VARCHAR,
+    court_count          INTEGER NOT NULL,
+    court_type           VARCHAR NOT NULL,
+    sport_id             INTEGER NOT NULL REFERENCES sport (id)
+);
+
+CREATE TABLE business_hour
+(
+    id         SERIAL PRIMARY KEY,
+    place_id   INTEGER    NOT NULL,
+    type       place_type NOT NULL,
+    weekday    INTEGER    NOT NULL, -- sunday = 7
+    start_time TIME       NOT NULL,
+    end_time   TIME       NOT NULL
+);
+
+
+CREATE TABLE album
+(
+    id        SERIAL PRIMARY KEY,
+    place_id  INTEGER    NOT NULL,
+    type      place_type NOT NULL,
+    file_uuid UUID       NOT NULL REFERENCES gcs_file (file_uuid)
+);
+
+
+CREATE TABLE court
+( -- 球場
+    id       SERIAL PRIMARY KEY,
+    venue_id INTEGER NOT NULL REFERENCES venue (id)
+);
+
+CREATE TABLE reservation
+( -- 預約
+    id              SERIAL PRIMARY KEY,
+    stadium_id      INTEGER REFERENCES stadium (id),
+    venue_id        INTEGER REFERENCES venue (id),
+    court_id        INTEGER REFERENCES court (id),
+    start_time      TIMESTAMP            NOT NULL,
+    end_time        TIMESTAMP            NOT NULL,
+    member_count    INTEGER              NOT NULL,
+    vacancy         INTEGER,
+    technical_level technical_type ARRAY NOT NULL,
+    remark          VARCHAR,
+    invitation_code VARCHAR              NOT NULL,
+    is_cancelled    BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE reservation_member
+(
+    reservation_id SERIAL REFERENCES reservation (id),
+    account_id     INTEGER REFERENCES account (id),
+    is_manager     BOOLEAN DEFAULT FALSE,
+    is_joined      BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE ban_record
+(
+    id         SERIAL PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES account (id),
+    event_id   INTEGER NOT NULL REFERENCES reservation (id),
+    start_date DATE    NOT NULL,
+    end_date   DATE    NOT NULL
+);
+
+CREATE TABLE notification
+(
+    id         SERIAL PRIMARY KEY,
+    account_id INTEGER   NOT NULL REFERENCES account (id),
+    event_id   INTEGER   NOT NULL REFERENCES reservation (id),
+    content    TEXT,
+    time       TIMESTAMP NOT NULL,
+    is_read    BOOLEAN DEFAULT FALSE
+);
