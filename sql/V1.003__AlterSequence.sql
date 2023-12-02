38cc17f5792bcded405bb97d062d8dfc354d8105
wsa <bensonwang2001@gmail.com>
2023-12-02 19:53:36 +0800

    feat: modify alter sequence's script

--- sql/V1.003__AlterSequence.sql
+++ sql/V1.003__AlterSequence.sql
@@ -1,9 +1,32 @@
-ALTER SEQUENCE account_id_seq RESTART WITH 4;
-ALTER SEQUENCE sport_id_seq RESTART WITH 6;
-ALTER SEQUENCE city_id_seq RESTART WITH 2;
-ALTER SEQUENCE stadium_id_seq RESTART WITH 6;
-ALTER SEQUENCE venue_id_seq RESTART WITH 11;
-ALTER SEQUENCE business_hour_id_seq RESTART WITH 51;
-ALTER SEQUENCE album_id_seq RESTART WITH 20;
-ALTER SEQUENCE court_id_seq RESTART WITH 37;
-ALTER SEQUENCE reservation_id_seq RESTART WITH 9;
+DO $$
+DECLARE
+    max_id INT;
+BEGIN
+    SELECT MAX(id) + 1 INTO max_id FROM account;
+    EXECUTE 'ALTER SEQUENCE account_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM sport;
+    EXECUTE 'ALTER SEQUENCE sport_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM city;
+    EXECUTE 'ALTER SEQUENCE city_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM stadium;
+    EXECUTE 'ALTER SEQUENCE stadium_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM venue;
+    EXECUTE 'ALTER SEQUENCE venue_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM business_hour;
+    EXECUTE 'ALTER SEQUENCE business_hour_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM album;
+    EXECUTE 'ALTER SEQUENCE album_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM court;
+    EXECUTE 'ALTER SEQUENCE court_id_seq RESTART WITH ' || max_id;
+
+    SELECT MAX(id) + 1 INTO max_id FROM reservation;
+    EXECUTE 'ALTER SEQUENCE reservation_id_seq RESTART WITH ' || max_id;
+
+END $$;
